f88628fb5742c7443851dd57709e26d84113cef1
Bobby <bobbyskhs@gmail.com>
2022-08-28 18:01:45 -0400

    Fix Languages Links

--- theme/js/page-loader.js
+++ theme/js/page-loader.js
@@ -12,16 +12,24 @@ const lang = getLocaleLanguge();
 const languages = []
 const languagesDiv = document.querySelector('.languages').getElementsByTagName('a');
 const page = window.location.pathname;
-const currentLanguage = window.location.pathname.slice(1, 3);
 
 for (let i = 0; i < languagesDiv.length; i++) {
     const language = languagesDiv[i].getAttribute('data-lang');
     languages.push(language);
-    const href =  '/' + language + page.slice(3);
+    let href = page.replace(page.split('/')[2], language);
     languagesDiv[i].setAttribute('href', href);
 }
 
-if (!languages.includes(currentLanguage)) {
-    window.location.href = '/' + lang + page;
+const isLocaleLoaded = (cl) => {
+  for (const language of languages) {
+    if (cl.includes(language)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+if (!isLocaleLoaded(page)) {
+    window.location.href = page + lang;
 }
 
